c9549fa8d007dcba3583bf94120cd7b2dfced6fc
janasolidade <janaina.solidade@gmail.com>
2024-01-17 18:16:08 -0300

    adicionando aulas sobre lista

---
+++ listas.py
@@ -0,0 +1,21 @@
+# Listas
+
+# fórmula: lista = []
+# lista.append ('maçã'): adiciona a maçã a lista
+# print(lista): exibe os itens da lista
+
+'''lista_frutas = [] # declaração da lista
+lista_frutas.append('Maçã')
+lista_frutas.append('Uva')
+lista_frutas.append('Banana')
+print(lista_frutas)'''
+
+
+#exemplo utilizando input de um usuário
+
+lista_frutas = [] # declaração da lista
+fruta = input('Digite sua fruta: ')
+lista_frutas.append(fruta)
+lista_frutas.append('Uva')
+lista_frutas.append('Banana')
+print(lista_frutas)
