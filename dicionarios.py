bd70ff4d23f20adb85f108ed12250a842ee937c4
janasolidade <janaina.solidade@gmail.com>
2024-01-17 18:31:20 -0300

    adicionando aula sobre dicionario

---
+++ dicionarios.py
@@ -0,0 +1,12 @@
+# Dicionários
+
+# dicionario = {}
+# print(dicionario)
+
+#Fórmula: dicionario = {'Chave':'Valor'}
+dicionario = {}
+dicionario['maca'] = 'É uma fruta'
+dicionario['carro'] = 'É um veículo'
+dicionario['gato'] = 'É um animal'
+
+print(dicionario)
