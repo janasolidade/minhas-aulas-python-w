125cca4a84e56709ef2ae8659f8198faebc07de5
janasolidade <janaina.solidade@gmail.com>
2024-01-17 18:00:06 -0300

    adicionando respostas para os dois primeiros exercicios

---
+++ exercicios_conceitos_basicos_python.py
@@ -0,0 +1,49 @@
+# 1) Faça um Programa que peça dois números, realize as principais operações soma, subtração, multiplicação, divisão.
+
+# Adição
+'''numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+resultado = (numero1+numero2)
+print('O resultado é ' f'{resultado}')'''
+
+# Subtração
+'''numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+resultado = (numero1-numero2)
+print('O resultado é ' f'{resultado}')'''
+
+
+# Multiplicação
+'''numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+resultado = (numero1*numero2)
+print('O resultado é ' f'{resultado}')'''
+
+
+# Divisão
+'''numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+resultado = (numero1//numero2)
+print('O resultado é ' f'{resultado}')'''
+
+
+
+# 2) Peça ao usuário para informar o ano de nascimento. Em seguida, calcule e imprima a idade atual.
+
+'''dataNascimento = int(input('Digite o seu ano de nascimento: '))
+anoAtual = 2024
+idade = anoAtual - dataNascimento
+print('A sua idade é ' f'{idade}')'''
+
+
+
+# 3) Faça um Programa que peça a quantidade de quilômetros, transforme em metros, centímetros e milímetros.
+
+
+
+kilometros = int(input('Digite a quantidade de quilômetros: '))
+
+
+
+
+
