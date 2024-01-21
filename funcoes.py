2fa6ba32ebf007a6abd2dea409727843faff131b
janasolidade <janaina.solidade@gmail.com>
2024-01-21 16:19:57 -0300

    adicionando aula sobre parâmetros

--- funcoes.py
+++ funcoes.py
@@ -4,21 +4,29 @@
  #   instruções ficam dentro da função
 # mostrar_mensagem() ----> chamada de função
 
-def soma(): # definição da função soma
-    calculo = 10+2
+def soma(num1, num2): # definição da função soma
+    
+    calculo = num1+num2
+
     print(f'O resultado da soma é: {calculo}')
 # soma() # chamando a função
 
 
-def subtracao():
+def subtracao(num1, num2):
     sub = 10-2
     print(f'O resultado da subtracao é: {sub}')
-    multiplicacao() # chamando função dentro de uma função
+    #multiplicacao() # chamando função dentro de uma função
 
 
-def multiplicacao():
+def multiplicacao(num1, num2):
     mult = 10*2
     print(f'O resultado da multiplicação é: {mult}')
 
-soma() # chamando a função
-subtracao()
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+soma(num1, num2) # chamando a função
+subtracao(num1, num2)
+multiplicacao(num1, num2)
+
+#Trabalhando com parâmetros
